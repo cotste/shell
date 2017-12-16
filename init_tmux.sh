2436827e5fcc053190efcee6829d7d334f6dcb6e
Stephen Cott <stephencott@gmail.com>
2017-12-16 08:42:21 -0800

    Minor changes to init_tmux.sh

--- init_tmux.sh
+++ init_tmux.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-tmux new -s main -n editor -d
+tmux new -s main -n terminal -d
+
+tmux neww -t main -n editor
 
 tmux send-keys -t editor "cd $1" C-m
 tmux send-keys -t editor "vim" C-m
 
-tmux splitw -t editor -h
-
 tmux neww -t main -n debug
-#tmux send-keys -t editor "gdb" C-m
 
 tmux splitw -t debug -h -b 
 tmux send-keys -t debug "tty > /tmp/source_win" C-m
 
 tmux splitw -t debug -v 
-tmux send-keys -t debug "tty > /tmp/mem_win" C-m
+tmux send-keys -t debug "tty > /tmp/asm_win" C-m
 
 tmux select-pane -t 2
 
@@ -22,7 +21,10 @@ tmux send-keys -t debug "gdb" C-m
 tmux send-keys -t debug C-m
 
 tmux send-keys -t debug "dashboard source -output $(</tmp/source_win)" C-m
-tmux send-keys -t debug "dashboard memory -output $(</tmp/mem_win)" C-m
+tmux send-keys -t debug "dashboard assembly -output $(</tmp/asm_win)" C-m
 tmux send-keys -t debug "cd $1" C-m
+
+tmux select-window -t terminal
+
 tmux attach -t main
 
