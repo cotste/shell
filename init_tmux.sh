0d2a9b408b25d0a3f7f15d734901f6d1a4e88fd0
Stephen Cott <stephen@loki.loc>
2017-12-18 19:14:30 -0800

    Started NERDTree with vim
    Specified the starting width and height of the initial tmux session to
    avoid NERDTree from freaking out. x=151,y=39 -- This will need to be
    changed depending on screen resolution.
    
    Also added a 20% split at the bottom of the editor window for compiling.

--- init_tmux.sh
+++ init_tmux.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-tmux new -s main -n terminal -d
+tmux start-server
 
-tmux neww -t main -n editor
+tmux new-session -s main -n terminal -d  -x 151 -y 39
 
-tmux send-keys -t editor "cd $1" C-m
-tmux send-keys -t editor "vim" C-m
+tmux new-window -t main -n editor "$(pwd); vim -c 'NERDTree'"
+
+#tmux send-keys -t editor "cd $1" C-m
+#tmux send-keys -t editor "vim -c 'NERDTree'" C-m
+#tmux send-keys -t editor ":NERDTree" C-m
+
+tmux split-window -t editor -p 20
+tmux select-pane -t 0
 
 tmux neww -t main -n debug
 
@@ -17,12 +23,15 @@ tmux send-keys -t debug "tty > /tmp/asm_win" C-m
 
 tmux select-pane -t 2
 
+tmux send-keys -t debug "cd $1" C-m
 tmux send-keys -t debug "gdb" C-m
 tmux send-keys -t debug C-m
 
 tmux send-keys -t debug "dashboard source -output $(</tmp/source_win)" C-m
 tmux send-keys -t debug "dashboard assembly -output $(</tmp/asm_win)" C-m
 tmux send-keys -t debug "cd $1" C-m
+
+tmux select-pane -t 0
 
 tmux select-window -t terminal
 
